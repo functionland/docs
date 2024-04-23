fd12d1698154750a99837695a4ab31e251cebdbe
Marco Rodriguez-Salinas <marco.rodriguez113@gmail.com>
2024-04-23 10:59:34 -0500

    included new file and adjusted paths accordingly to the moved directories

--- sidebars.js
+++ sidebars.js
@@ -43,8 +43,13 @@ const sidebars = {
       ]
     },
     {
+      type: 'doc',
+      label: 'Getting Started With FxBlox',
+      id: 'getting-started-testnet',
+    },
+    {
       type: 'category',
-      label:'Functionyard Testnet',
+      label: 'Functionyard Testnet',
       link: {
         type:'doc',
         id:'functionyard/index',
@@ -83,11 +88,11 @@ const sidebars = {
           label:'Getting Started',
           link: {
             type:'doc',
-            id:'getting-started',
+            id:'api/getting-started/index',
           },
           items:[
-            'getting-started/box-setup',
-            'getting-started/using-samples',
+            'api/getting-started/box-setup',
+            'api/getting-started/using-samples',
           ]
         },
         {
@@ -98,7 +103,7 @@ const sidebars = {
             id:'reference-api',
           },
           items:[
-              'api/client-instance',
+            'api/client-instance',
             'api/graph-api',
             'api/file-api'
           ]
